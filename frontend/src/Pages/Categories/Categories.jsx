48bc1e3f154190df366b473dd56e9e205da70eca
TreceUA <sergiomaringua@gmail.com>
2025-05-21 12:05:03 +0200

    categorias actualizado.

--- frontend/src/Pages/Categories/Categories.jsx
+++ frontend/src/Pages/Categories/Categories.jsx
@@ -19,13 +19,6 @@ function Categories() {
 
     return (
         <div className={styles["category-main-container"]}>
-            <section className={styles["type-section"]}>
-                <Button variant="red-rounded" label="3D" to="/home"/>
-                <Button variant="red-rounded" label="2D" to="/home"/>
-                <Button variant="red-rounded" label="Vídeo" to="/home"/>
-                <Button variant="red-rounded" label="Audio" to="/home"/>
-                <Button variant="red-rounded" label="Script" to="/home"/>
-            </section>
             <h2 className={styles["category-title"]}>Categorías</h2>
             <section className={styles["category-grid"]}>
                 {categories.map((category) => (
